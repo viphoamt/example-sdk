e5a2d57398e2bb0cf0393c0c4fd7821e646ee6e7
Nguyen Van Hoa <hoanv3@tinhvan.com>
2021-04-29 14:00:07 +0700

    continue

---
+++ CreateSdkTest/src/main/java/com/example/createsdktest/HelloWorld.kt
@@ -0,0 +1,7 @@
+package com.example.createsdktest
+
+class HelloWorld {
+    fun helloWorld(): String? {
+        return "Hello world \n I'm Viphoamt"
+    }
+}
--- app/src/main/java/com/example/examplesdk/MainActivity.kt
+++ app/src/main/java/com/example/examplesdk/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.examplesdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.createsdktest.HelloWorld
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloWorld = HelloWorld()
+
+        val txtText = findViewById<TextView>(R.id.txtHello)
+        txtText.text = helloWorld.helloWorld()
     }
 }
